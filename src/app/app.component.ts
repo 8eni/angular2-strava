0e6535f3d2da6461fa1a92d2c51f59799594bdb4
Liching Li <liching.li@rea-group.com>
2017-05-19 14:41:07 +1000

    Connect strava api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { StravaService } from './strava.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  errorMessage: string;
+  athlete: any;
+  stats: any;
+  friends: any;
+
+  constructor(private http: Http, private stravaService: StravaService) {}
+
+  ngOnInit() {
+    this.getAthlete();
+  }
+
+  getAthlete() {
+    this.stravaService.getAthlete()
+                      .subscribe(
+                        athlete =>  {
+                          this.athlete = athlete;
+                          this.getStats(athlete.id);
+                          this.getFriends(athlete.id);
+                        },
+                        error => this.errorMessage = <any>error);
+  }
+
+  getStats(id) {
+    return new Promise((resolve, reject) => {
+      this.stravaService.getStats(id)
+                      .subscribe(
+                        stats => this.stats = stats,
+                        error => this.errorMessage = <any>error);
+    });
+  }
+
+  getFriends(id) {
+    return new Promise((resolve, reject) => {
+      this.stravaService.getFriends(id)
+                      .subscribe(
+                        friends => this.friends = friends,
+                        error => this.errorMessage = <any>error);
+    });
+  }
 }
